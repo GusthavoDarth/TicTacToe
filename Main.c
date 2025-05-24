07e4a07e21e73f5e0dee64a73f24a18161ea6cf8
gusth <gusthavo.goncalves@gmail.com>
2025-05-24 01:30:52 -0300

    commit

--- Main.c
+++ Main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Src\Functions.h"
 
 
 
 void main()
 {
+    char turn = 'X';
+    char** board = createBoard();
     intro();
+    int mode = gameMode();
+
+ 
+    switch (mode)
+    {
+    case 1:
+        printf("Let the game begin!\n");
+        drawBoard(board);
+
+        while (winCon(board, turn) == 0)
+        {
+            inputBoard(board,turn);
+            turn = swapTurn(turn);
+        }
+        break;
+    
+    case 2:
+        printf("Let the game begin!\n");
+        drawBoard(board);
+
+        while (winCon(board, turn) == 0)
+        {
+            inputBoard(board, turn);
+            turn = swapTurn(turn);
+            randomBot(board, turn);
+            turn = swapTurn(turn);
+        }
+        break;
+    
+    default:
+        break;
+    }
+   
+
 
 }
